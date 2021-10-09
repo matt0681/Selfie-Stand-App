4b5ea480aa2c246a10afd25c0b71787bcc01c450
Matthew Lad <37879186+matt0681@users.noreply.github.com>
2021-10-08 22:10:25 -0500

    Massive Changes: Added a Navigation Bar and organized code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';  // Imports Dart.
 
+
 /// This function runs the entire app.
-///
 void main() {
   runApp(const MaterialApp(
     // Uses our home screen defined below
@@ -11,194 +10,156 @@ void main() {
   ));
 }
 
+/// This is the main application widget.
+/// It sends us straight to the Home screen.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String title = "Selfie Stand App";
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: title,
+      home: Home(),
+    );
+  }
+}
+
 /// Represents all content shown on the Home screen.
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<Home> createState() => HomeState();
+}
 
-    // The Scaffold is a structural/organizational widget which holds other components.
-    return Scaffold(
-      // The App Bar widget appears at the top of the app
-      appBar: AppBar(
+/// Represents the state of the Home screen.
+class HomeState extends State<Home> {
+  // The current / starting page.
+  int selectedIndex = 1;
 
-        // Title property - text shown at the top
-        title: const Text(
-          "Home",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.white,
-            fontFamily: 'HeptaSlab',
-          ),
-        ),
+  // List of navigation bar widgets
+  static const List<Widget> widgetOptions = <Widget>[
+    Selfies_Widget(),
+    Map_Widget(),
+    Friends_Widget(),
+    Account_Widget(),
+  ];
 
-        // Describes the account button and settings button at the top right of the appbar.
-        actions: <Widget>[
-          // Button to go to the list of locations screen.
-          IconButton(
-            icon: const Icon(Icons.list),
-            tooltip: 'Locations',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) => const Locations(),
-                ),
-              ); // Navigator push
-            },
-          ),
-
-          // Button to go to the settings screen.
-          IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: 'Settings',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) => const Settings(),
-                ),
-              ); // Navigator push
-            }, // onPressed
-          ),
-        ],
-
-        // The color of the appbar Background.
-        backgroundColor: Colors.cyan[600],
-      ),
-
-      // Defines all content under the app bar.
-      // The Center widget centers everything within it.
-      body: const Center(
-        // The Text widget contains a string and a styling widget.
-        child: Text(
-          "GPS MAP",
-          style: TextStyle(
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.black,
-            fontFamily: 'HeptaSlab',
-
-          ),
-        ),
-      ),
-
-      backgroundColor: Colors.cyan[100],
-
-      // Account Button Widget
-      floatingActionButton: FloatingActionButton(
-        // Action to perform on press
-        onPressed: () {},
-
-        // Styling
-        backgroundColor:  Colors.cyan[400],
-
-
-      ),
-    );
-  } // build
-} // Home
-
-
-/// Represents all content shown on the Settings screen.
-class Settings extends StatelessWidget {
-  const Settings({Key? key}) : super(key: key);
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     // The Scaffold is a structural/organizational widget which holds other components.
     return Scaffold(
-      // The App Bar widget appears at the top of the app
-      appBar: AppBar(
 
-        // Title property - text shown at the top
-        title: const Text(
-          "Settings",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.white,
-            fontFamily: 'HeptaSlab',
-          ),
-        ),
-
-        // The color of the appbar Background.
-        backgroundColor: Colors.cyan[600],
+      // This code displays something on the screen, based on what item is pressed at the navigation bar.
+      body: Center(
+        child: widgetOptions.elementAt(selectedIndex),
       ),
 
-      // Defines all content under the app bar.
-      // The Center widget centers everything within it.
-      body: const Center(
-        // The Text widget contains a string and a styling widget.
-        child: Text(
-          "Settings go here",
-          style: TextStyle(
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.black,
-            fontFamily: 'HeptaSlab',
+      // The bar along the bottom of the app with buttons on it.
+      bottomNavigationBar: BottomNavigationBar(
+        // Various settings which apply to look/feel.
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.cyan[900],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        // What to do when an item is pressed in the bar.
+        onTap: onItemTapped,
+        currentIndex: selectedIndex,
+        // The items contained in the bar.
+        items: const [
+          BottomNavigationBarItem(
+            label: "Selfies",
+            icon: Icon(Icons.photo_library),
           ),
-        ),
-      ),
-
-      backgroundColor: Colors.cyan[100],
+          BottomNavigationBarItem(
+            label: "Map",
+            icon: Icon(Icons.explore),
+          ),
+          BottomNavigationBarItem(
+            label: "Friends",
+            icon: Icon(Icons.people),
+          ),
+          BottomNavigationBarItem(
+            label: "Account",
+            icon: Icon(Icons.account_circle),
+          ),
+        ],  // items
+      )
     );
-  } // build
-} // Home
+  }
+}
 
 
-/// Represents all content shown on the Settings screen.
-class Locations extends StatelessWidget {
-  const Locations({Key? key}) : super(key: key);
+
+/// Represents all content shown on the Account screen.
+class Selfies_Widget extends StatelessWidget {
+  const Selfies_Widget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    // The Scaffold is a structural/organizational widget which holds other components.
-    return Scaffold(
-      // The App Bar widget appears at the top of the app
-      appBar: AppBar(
-
-        // Title property - text shown at the top
-        title: const Text(
-          "Your Locations",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.white,
-            fontFamily: 'HeptaSlab',
-          ),
-        ),
-
-        // The color of the appbar Background.
-        backgroundColor: Colors.cyan[600],
+    return const Scaffold(
+      body: Center(
+        child: Text("Selfies"),
       ),
-
-      // Defines all content under the app bar.
-      // The Center widget centers everything within it.
-      body: const Center(
-        // The Text widget contains a string and a styling widget.
-        child: Text(
-          "List of your locations/ pictures",
-          style: TextStyle(
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.black,
-            fontFamily: 'HeptaSlab',
-          ),
-        ),
-      ),
-
-      backgroundColor: Colors.cyan[100],
     );
-  } // build
-} // Home
+  }
+}
+
+
+
+/// Represents all content shown on the Account screen.
+class Map_Widget extends StatelessWidget {
+  const Map_Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Map"),
+      ),
+    );
+  }
+}
+
+
+
+/// Represents all content shown on the Account screen.
+class Friends_Widget extends StatelessWidget {
+  const Friends_Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Friends"),
+      ),
+    );
+  }
+}
+
+
+
+/// Represents all content shown on the Account screen.
+class Account_Widget extends StatelessWidget {
+  const Account_Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Settings"),
+      ),
+    );
+  }
+}
+
